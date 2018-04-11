e54ac4aa81aa42dffe61fbd663d5020e9386e7e2
soknath Mil <soknath.siit@gmail.com>
2018-04-11 10:18:17 +0700

    first commit

--- android/app/src/main/java/com/lottoapp/MainActivity.java
+++ android/app/src/main/java/com/lottoapp/MainActivity.java
@@ -1,6 +1,8 @@
 package com.lottoapp;
 
 import com.facebook.react.ReactActivity;
+import org.devio.rn.splashscreen.SplashScreen; // for splash screen
+import android.os.Bundle; // for splash screen
 
 public class MainActivity extends ReactActivity {
 
@@ -11,5 +13,11 @@ public class MainActivity extends ReactActivity {
     @Override
     protected String getMainComponentName() {
         return "LottoApp";
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        SplashScreen.show(this);  // here
+        super.onCreate(savedInstanceState);
     }
 }
